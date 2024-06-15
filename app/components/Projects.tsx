c46851a78e9ce865de3d6c38aedc0fe2a11fcb8d
Marius Tetlie <marius.tetlie@gmail.com>
2024-06-15 10:43:53 +0200

    style: project image transition

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -6,13 +6,17 @@ import { CanvasActionsContext } from './Context'
 import ProjectNavigation from './ProjectNavigation'
 import ProjectImage from './ProjectImage'
 
+import { motion, AnimatePresence, type Variants } from 'framer-motion'
+
 export default function Projects({ projects }: { projects: Project[] }) {
-  const { setFooterText } = useContext(CanvasActionsContext)
+  const { setFooterText, setProjectDescription, setDescriptionIsOpen } =
+    useContext(CanvasActionsContext)
 
   const projectImages = useMemo(() => {
     return projects
       .flatMap((project) =>
-        project.projectImages?.map((image) => ({
+        project.projectImages?.map((image, index) => ({
+          index,
           title: project.title,
           text: project.text,
           asset: image.image.asset,
@@ -27,25 +31,49 @@ export default function Projects({ projects }: { projects: Project[] }) {
 
   const handleNext = () => {
     setCurrentIndex((currentIndex + 1) % projectImages.length)
+    setDescriptionIsOpen(false)
   }
 
   const handlePrev = () => {
     setCurrentIndex((currentIndex - 1 + projectImages.length) % projectImages.length)
+    setDescriptionIsOpen(false)
   }
 
   useEffect(() => {
-    if (currentImage?.text) {
-      setFooterText(currentImage.text)
+    if (currentImage?.title) {
+      setFooterText(currentImage.title)
     }
-  }, [currentImage, setFooterText])
+    if (currentImage?.text) {
+      setProjectDescription(currentImage.text)
+    }
+  }, [currentImage.title, currentImage.text, setFooterText, setProjectDescription])
 
   if (!currentImage) {
     return null
   }
 
+  const textAnimation: Variants = {
+    initial: { opacity: 0, y: 10 },
+    animate: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -10 },
+  }
+
   return (
-    <div className="relative h-full max-h-max w-full overflow-hidden flex items-center justify-center">
-      <ProjectImage asset={currentImage.asset} layout={currentImage.layout} />
+    <div className="relative h-full max-h-max w-full overflow-hidden ">
+      <AnimatePresence mode="wait">
+        <motion.div
+          className="w-full h-full relative flex items-center justify-center"
+          key={currentImage.index}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={textAnimation}
+          transition={{ duration: 0.2 }}
+        >
+          <ProjectImage asset={currentImage.asset} layout={currentImage.layout} />
+        </motion.div>
+      </AnimatePresence>
+
       <ProjectNavigation onNext={handleNext} onPrev={handlePrev} />
     </div>
   )
