d6f43e3500d3661a3abf1ed29697ef6a85a4220a
Marius Tetlie <marius.tetlie@gmail.com>
2024-06-15 10:43:34 +0200

    feat: project description toggle

--- app/components/Context.tsx
+++ app/components/Context.tsx
@@ -8,21 +8,24 @@ import {
   ReactNode,
   Dispatch,
   SetStateAction,
-  useEffect,
 } from 'react'
 import { PortableTextBlock } from 'sanity'
 
 export type CanvasStateType = {
   hasDrawn: boolean
   canvasIsOpen: boolean
-  footerText: PortableTextBlock[]
+  descriptionIsOpen: boolean
+  footerText: string
+  projectDescription: PortableTextBlock[]
 }
 
 export type CanvasActionsType = {
   setHasDrawn: Dispatch<SetStateAction<boolean>>
   setCanvasIsOpen: Dispatch<SetStateAction<boolean>>
   toggleCanvas: () => void
-  setFooterText: Dispatch<SetStateAction<PortableTextBlock[]>>
+  setFooterText: Dispatch<SetStateAction<string>>
+  setDescriptionIsOpen: Dispatch<SetStateAction<boolean>>
+  setProjectDescription: Dispatch<SetStateAction<PortableTextBlock[]>>
   clearCanvas: () => void
   setClearCanvas: Dispatch<SetStateAction<() => void>>
 }
@@ -30,14 +33,18 @@ export type CanvasActionsType = {
 export const CanvasStateContext = createContext<CanvasStateType>({
   hasDrawn: false,
   canvasIsOpen: false,
-  footerText: [],
+  descriptionIsOpen: false,
+  footerText: 'Loading...',
+  projectDescription: [],
 })
 
 export const CanvasActionsContext = createContext<CanvasActionsType>({
   setHasDrawn: () => {},
   setCanvasIsOpen: () => {},
+  setDescriptionIsOpen: () => {},
   toggleCanvas: () => {},
   setFooterText: () => {},
+  setProjectDescription: () => {},
   clearCanvas: () => {},
   setClearCanvas: () => {},
 })
@@ -45,33 +52,42 @@ export const CanvasActionsContext = createContext<CanvasActionsType>({
 export function AppWrapper({ children }: { children: ReactNode }) {
   const [hasDrawn, setHasDrawn] = useState<boolean>(false)
   const [canvasIsOpen, setCanvasIsOpen] = useState<boolean>(false)
-  const [footerText, setFooterText] = useState<PortableTextBlock[]>([])
+  const [descriptionIsOpen, setDescriptionIsOpen] = useState<boolean>(false)
+  const [footerText, setFooterText] = useState<string>('Loading...')
+  const [projectDescription, setProjectDescription] = useState<PortableTextBlock[]>([])
   const [clearCanvas, setClearCanvas] = useState<() => void>(() => () => {})
 
   const toggleCanvas = useCallback(() => {
     setCanvasIsOpen((prevState) => !prevState)
+    setDescriptionIsOpen(false)
   }, [])
 
   const state = useMemo(
-    () => ({ hasDrawn, canvasIsOpen, footerText }),
-    [hasDrawn, canvasIsOpen, footerText]
+    () => ({ hasDrawn, canvasIsOpen, descriptionIsOpen, footerText, projectDescription }),
+    [hasDrawn, canvasIsOpen, descriptionIsOpen, footerText, projectDescription]
   )
   const actions = useMemo(
     () => ({
       setHasDrawn,
       setCanvasIsOpen,
+      setDescriptionIsOpen,
       toggleCanvas,
       setFooterText,
+      setProjectDescription,
       clearCanvas,
       setClearCanvas,
     }),
-    [setHasDrawn, setCanvasIsOpen, toggleCanvas, setFooterText, clearCanvas, setClearCanvas]
+    [
+      setHasDrawn,
+      setCanvasIsOpen,
+      setDescriptionIsOpen,
+      toggleCanvas,
+      setFooterText,
+      setProjectDescription,
+      clearCanvas,
+      setClearCanvas,
+    ]
   )
-
-  useEffect(() => {
-    console.log('Canvas is open:', canvasIsOpen)
-  }),
-    [canvasIsOpen]
 
   return (
     <CanvasStateContext.Provider value={state}>
--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,19 +6,26 @@ import { motion, AnimatePresence, Variants } from 'framer-motion'
 import { CanvasActionsContext, CanvasStateContext } from './Context'
 import { PortableTextComponents } from './PortableTextComponents'
 import { PortableText } from '@portabletext/react'
-import type { PortableTextBlock } from 'sanity'
+import { type PortableTextBlock } from 'sanity'
 
 export default function Footer({ globals }: { globals: Globals }) {
-  const { footerText, canvasIsOpen, hasDrawn } = useContext(CanvasStateContext)
-  const { toggleCanvas, clearCanvas } = useContext(CanvasActionsContext)
+  const { footerText, projectDescription, descriptionIsOpen, canvasIsOpen, hasDrawn } =
+    useContext(CanvasStateContext)
+  const { toggleCanvas, clearCanvas, setDescriptionIsOpen } = useContext(CanvasActionsContext)
   const [displayedFooterText, setDisplayedFooterText] = useState(footerText)
+  const [displayedProjectDescription, setDisplayedProjectDescription] = useState<
+    PortableTextBlock[]
+  >([])
   const [isAnimating, setIsAnimating] = useState(false)
+
+  const toggleDescription = () => setDescriptionIsOpen((prevState) => !prevState)
 
   useEffect(() => {
     if (!isAnimating) {
       setDisplayedFooterText(footerText)
+      setDisplayedProjectDescription(projectDescription)
     }
-  }, [footerText, isAnimating])
+  }, [footerText, projectDescription, isAnimating])
 
   const handleAnimationStart = () => {
     setIsAnimating(true)
@@ -27,8 +34,10 @@ export default function Footer({ globals }: { globals: Globals }) {
   const handleAnimationComplete = () => {
     setIsAnimating(false)
     setDisplayedFooterText(footerText)
+    setDisplayedProjectDescription(projectDescription)
   }
 
+  const slugify = (text: string) => text.toLowerCase().split(' ').join('-')
   const generateKey = (text: PortableTextBlock[]) => text.map((block) => block._key).join('-')
 
   const textAnimation: Variants = {
@@ -37,87 +46,108 @@ export default function Footer({ globals }: { globals: Globals }) {
     exit: { opacity: 0, y: -10 },
   }
   return (
-    <footer className="w-full leading-none transition-height duration-150 ease-in text-sm md:text-lg relative flex items-end md:items-center z-30 justify-between px-2 bg-white md:px-4 lg:px-8 py-3 lg:py-4">
-      <AnimatePresence mode="wait">
-        {canvasIsOpen &&
-          (hasDrawn ? (
-            <motion.button
-              key="clearCanvas"
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              transition={{ duration: 0.2 }}
-              variants={textAnimation}
-              type="button"
-              onClick={clearCanvas}
-              className="underline-offset-2 decoration-1 decoration-black"
-            >
-              <span className="md:hidden">
-                &nbsp;
-                <br />
-              </span>
-              Clear canvas
-            </motion.button>
-          ) : (
-            <motion.span
-              key="drawSomething"
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              transition={{ duration: 0.2 }}
-              variants={textAnimation}
-            >
-              <span className="md:hidden">
-                &nbsp;
-                <br />
-              </span>
-              {globals?.drawingBoardText ? (
-                <PortableText
-                  value={globals.drawingBoardText}
-                  components={PortableTextComponents}
-                />
-              ) : (
-                'Draw something.'
-              )}
-            </motion.span>
-          ))}
-        {!canvasIsOpen && (
-          <>
-            {displayedFooterText.length > 0 ? (
-              <motion.div
-                key={generateKey(displayedFooterText)}
+    <footer className="relative">
+      <div
+        id="description"
+        className={`leading-snug pb-[57px] md:pb-[61px] pointer-events-auto bottom-0 pt-4 lg:pt-4 bg-white px-2 md:px-4 lg:px-8 absolute left-0 h-auto overflow-y-scroll w-screen transition-transform duration-300 ease-in-out z-20 ${
+          descriptionIsOpen ? 'transform-none' : 'transform translate-y-[100vh]'
+        }`}
+      >
+        <motion.div
+          key={generateKey(displayedProjectDescription)}
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={textAnimation}
+          onAnimationStart={handleAnimationStart}
+          onAnimationComplete={handleAnimationComplete}
+        >
+          <div className="max-w-[80vw]">
+            <PortableText value={displayedProjectDescription} components={PortableTextComponents} />
+          </div>
+        </motion.div>
+      </div>
+      <div className="w-full leading-none transition-height duration-150 ease-in relative flex items-center z-30 justify-between px-2 bg-white md:px-4 lg:px-8 py-3 lg:py-4">
+        <AnimatePresence mode="wait">
+          {canvasIsOpen &&
+            (hasDrawn ? (
+              <motion.button
+                key="clearCanvas"
                 initial="initial"
                 animate="animate"
                 exit="exit"
+                transition={{ duration: 0.2 }}
                 variants={textAnimation}
-                onAnimationStart={handleAnimationStart}
-                onAnimationComplete={handleAnimationComplete}
+                type="button"
+                onClick={clearCanvas}
+                className="underline-offset-2 decoration-1 decoration-black"
               >
-                <div className="max-w-[80vw]">
-                  <PortableText value={displayedFooterText} components={PortableTextComponents} />
-                </div>
-              </motion.div>
-            ) : (
-              <span>
-                {' '}
                 <span className="md:hidden">
                   &nbsp;
                   <br />
                 </span>
-                Loading project...
-              </span>
-            )}
-          </>
-        )}
-      </AnimatePresence>
-      <div className="flex gap-2 md:gap-4">
-        <button
-          type="button"
-          aria-label={canvasIsOpen ? 'Close canvas' : 'Open canvas'}
-          onClick={toggleCanvas}
-        >
-          <div className="size-[20px] bg-black rounded-full"></div>
-        </button>
+                Clear canvas
+              </motion.button>
+            ) : (
+              <motion.span
+                key="drawSomething"
+                initial="initial"
+                animate="animate"
+                exit="exit"
+                transition={{ duration: 0.2 }}
+                variants={textAnimation}
+              >
+                <span className="md:hidden">
+                  &nbsp;
+                  <br />
+                </span>
+                {globals?.drawingBoardText ? (
+                  <PortableText
+                    value={globals.drawingBoardText}
+                    components={PortableTextComponents}
+                  />
+                ) : (
+                  'Draw something.'
+                )}
+              </motion.span>
+            ))}
+          {!canvasIsOpen && (
+            <motion.div
+              key={slugify(displayedFooterText)}
+              initial="initial"
+              animate="animate"
+              exit="exit"
+              variants={textAnimation}
+              onAnimationStart={handleAnimationStart}
+              onAnimationComplete={handleAnimationComplete}
+            >
+              <div className="max-w-[80vw]">
+                <button
+                  onClick={toggleDescription}
+                  aria-label={descriptionIsOpen ? 'Hide description' : 'Show description'}
+                  type="button"
+                >
+                  <span
+                    className={`block text-left transition-transform duration-500 ease-in-out underline-offset-2 decoration-1 decoration-black ${
+                      descriptionIsOpen ? 'underline ' : 'hover:underline '
+                    }`}
+                  >
+                    {displayedFooterText}
+                  </span>
+                </button>
+              </div>
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <div className="flex gap-2 md:gap-4">
+          <button
+            type="button"
+            aria-label={canvasIsOpen ? 'Close canvas' : 'Open canvas'}
+            onClick={toggleCanvas}
+          >
+            <div className="size-[20px] bg-black rounded-full"></div>
+          </button>
+        </div>
       </div>
     </footer>
   )
